42b9bacfaa3ef4e9a658cebfce4addb8148f5d0d
Andrew <ukperformancehq@gmail.com>
2026-04-06 17:26:56 +0300

    Fix admin plans table: remove fixed minWidth for mobile responsiveness

--- app/routes/app.admin.plans.tsx
+++ app/routes/app.admin.plans.tsx
@@ -716,7 +716,7 @@ export default function AdminPlans() {
                   <Divider />
 
                   <div style={{ overflowX: "auto" }}>
-                    <table style={{ width: "100%", borderCollapse: "collapse", minWidth: "800px" }}>
+                    <table style={{ width: "100%", borderCollapse: "collapse" }}>
                       <thead>
                         <tr>
                           {["Plan", "Price", "Products", "Fitments", "Providers", "Makes", "Fetches", "Features", "Badge"].map((h) => (
